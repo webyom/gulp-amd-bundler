6fb7e0925fd4a5d5f66166eeee0fd7139fbdb655
webyom@gmail.com <webyom@gmail.com>
2015-05-28 10:36:32 +0800

    update

---
+++ example/src/mod-x.jsx
@@ -0,0 +1,13 @@
+var ModX = react.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+				<p>
+					Hello, world! I am a CommentBox.
+				</p>
+			</div>
+		);
+	}
+});
+
+module.exports = ModA;
